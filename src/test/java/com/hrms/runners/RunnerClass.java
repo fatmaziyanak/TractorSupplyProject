7d324dcab3fbef9d4579026ee986126952b19065
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-01-26 15:14:30 -0500

    All class code

--- src/test/java/com/hrms/runners/RunnerClass.java
+++ src/test/java/com/hrms/runners/RunnerClass.java
@@ -8,6 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features="src/test/resources/features/Login.feature"
+		,glue="com/hrms/steps"
+		,dryRun=false
 		)
 
 public class RunnerClass {
--- src/test/java/com/hrms/steps/LoginSteps.java
+++ src/test/java/com/hrms/steps/LoginSteps.java
@@ -1,5 +1,7 @@
 package com.hrms.steps;
 
+import org.junit.Assert;
+
 import com.hrms.pages.LoginPageElements;
 import com.hrms.testbase.BaseClass;
 import com.hrms.utils.CommonMethods;
@@ -14,7 +16,7 @@ public class LoginSteps extends CommonMethods{
 	
 	@Given("I open browser and navigated to the HRMS")
 	public void i_open_browser_and_navigated_to_the_HRMS() {
-	   BaseClass.setUp();
+	   setUp();
 	}
 
 	@When("I enter valid username and valid password")
@@ -32,5 +34,23 @@ public class LoginSteps extends CommonMethods{
 	@Then("I successfully logged in")
 	public void i_successfully_logged_in() {
 	    System.out.println("I am logged in");
+	}
+	
+	@When("I enter valid username and invalid password")
+	public void i_enter_valid_username_and_invalid_password() {
+	    login=new LoginPageElements();
+		sendText(login.username, "Admin");
+	    sendText(login.password, "66jhhj");
+	}
+
+	@Then("I see error message")
+	public void i_see_error_message() {
+	   boolean error=login.errorMsg.isDisplayed();
+	   Assert.assertTrue("Error message is not displayed", error);
+	}
+	
+	@Then("I close browser")
+	public void i_close_browser() {
+	    tearDown();
 	}
 }
