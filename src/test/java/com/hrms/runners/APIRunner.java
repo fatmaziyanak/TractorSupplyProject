d988248632c9234ecb7f9ce35c88e244f6e9cc72
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-02-29 11:02:56 -0500

    API changes

--- src/test/java/com/hrms/runners/APIRunner.java
+++ src/test/java/com/hrms/runners/APIRunner.java
@@ -7,8 +7,8 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
                  glue = {"com.hrms.API.steps.practice"},
-                 dryRun = false,
-                 tags = "@APITEST")
+                 dryRun = true,
+                 tags = "@SyntaxHRMSAPIEndToEnd")
 
 public class APIRunner {
 
