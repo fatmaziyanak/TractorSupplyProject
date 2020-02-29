8b93775ba2d6cb25cbefa20463e660b85a62fc91
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-02-29 13:59:44 -0500

    pushing updates

--- src/test/java/com/hrms/API/steps/practice/GETOneEmployeeAPISteps.java
+++ src/test/java/com/hrms/API/steps/practice/GETOneEmployeeAPISteps.java
@@ -7,7 +7,7 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.equalTo;
 
 import org.junit.Assert;
 
@@ -16,58 +16,52 @@ import com.hrms.utils.APIConstants;
 public class GETOneEmployeeAPISteps {
 	private Response response;
 	private static RequestSpecification request;
-	
+
 	@Given("user calls getOneEmployee API")
 	public void user_calls_getOneEmployee_API() {
-		
-		request = given().header("Content-Type","application/json").header("Authorization", SyntaxAPIAuthenticationSteps.Token)
-		.param("employee_id", POSTCreateEmployeeAPI.employee_ID);
-	
-	
+
+		request = given().header("Content-Type", "application/json")
+				.header("Authorization", SyntaxAPIAuthenticationSteps.Token)
+				.param("employee_id", POSTCreateEmployeeAPI.employee_ID);
+
 	}
 
 	@When("User retrieves response for getOneEmployee API")
 	public void user_retrieves_response_for_getOneEmployee_API() {
-		
+
 		response = request.when().get(APIConstants.GET_ONE_EMPLOYEE_URI);
 		response.prettyPrint();
-		
+
 	}
 
 	@Then("status code is {int} for getOneEmployee API")
 	public void status_code_is_for_getOneEmployee_API(int int1) {
-	
+
 		response.then().assertThat().statusCode(int1);
-	
-	
+
 	}
 
 	@Then("user validates created employee exists")
 	public void user_validates_created_employee_exists() {
-		
+
 		response.then().body("employee[0].employee_id", equalTo(POSTCreateEmployeeAPI.employee_ID));
-		
-		
+
 		JsonPath jsonPathEvaluator = response.jsonPath();
-		
-	
+
 		String actualyEmployeeID = jsonPathEvaluator.get("employee[0].employee_id");
-		
+
 		System.out.println("Actual Employee ID is:" + actualyEmployeeID);
-		
-		
-		//Assert.assertEquals("Verifying Employee IDs", POSTCreateEmployeeAPI.employee_ID, actualyEmployeeID);
-		
-		
+
+		Assert.assertEquals("Verifying Employee IDs", POSTCreateEmployeeAPI.employee_ID, actualyEmployeeID);
+
 		try {
 			Assert.assertEquals("Verifying Employee IDs", POSTCreateEmployeeAPI.employee_ID, actualyEmployeeID);
-			
-		}catch(AssertionError e) {
-			
+
+		} catch (AssertionError e) {
+
 			System.out.println("Employee ID's DO NOT MATCH");
 		}
 		System.out.println("EMPLOYEE ID's MATCH");
 	}
-	
 
 }
