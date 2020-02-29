26f904d028e1c87574a7ef4597a13a7263beaa99
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-02-29 13:12:50 -0500

    updates

--- src/test/java/com/hrms/utils/APIConstants.java
+++ src/test/java/com/hrms/utils/APIConstants.java
@@ -3,6 +3,7 @@ package com.hrms.utils;
 public class APIConstants {
 
 	public static final String CREATE_EMPLOYEE_URI = "http://18.232.148.34/syntaxapi/api/createEmployee.php";
+	public static final String GET_ONE_EMPLOYEE_URI = "http://18.232.148.34/syntaxapi/api/getOneEmployee.php";
 	
 	
 	public static final String CREATE_EMPLOYEE_JSON = System.getProperty("user.dir")+ "/src/test/resources/JSONFiles/createEmployee.json";
